06623bb0fe0e6e415f42f8d208ca3f4f66a6ab62
Mafer Zanguetim <mafer.zanguetimcontatos@gmail.com>
2022-04-04 16:45:27 +0000

    Exercicio 2

---
+++ Exercicio2.java
@@ -0,0 +1,12 @@
+public class Exercicio2{
+    public static void main(String[] args){
+        int numeros = 100;
+
+        for (int i=0; i<numeros; i++) {
+            if (i%6 == 0){
+                System.out.print(i+" ");
+            }
+
+        }    
+    }
+}
