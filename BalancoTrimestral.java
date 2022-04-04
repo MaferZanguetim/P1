6b977fc1dfa6f8ed3ca2fc539c99d02af6b8c597
Mafer Zanguetim <mafer.zanguetimcontatos@gmail.com>
2022-04-04 16:34:25 +0000

    Exercicio 1

---
+++ BalancoTrimestral.java
@@ -0,0 +1,8 @@
+public class BalancoTrimestral{
+    public static void main(String[] args){
+        int x;
+        int gastosAbril = 1212;
+        x = gastosAbril*10;
+        System.out.println(x);
+    }
+}
